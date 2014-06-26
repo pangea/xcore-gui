086ac873cf0695efacc36c106dc6f3cdfc8d6f88
Noah Prince <noahprince8@gmail.com>
2014-06-26 18:32:45 -0500

    Wrote test framework. Broken still

--- tests/lib/login.js
+++ tests/lib/login.js
@@ -1,12 +1,69 @@
 (function() {
-  var Browser = require("zombie");
-  var assert = require("assert");
-  XV = {};
-  
-  if (!XV.browser) {
-    XV.browser = new Browser();  
-  }
 
-  // Load the page from localhost
-  XV.browser.visit("http://localhost:8443/");
-}());
+   var Zombie = require("zombie"),
+   assert = require("assert"),
+   secondsToWait = 40;
+   XZ = {};
+
+   var loadApp = exports.loadApp = function (options) {
+
+	   // Load the page from localhost
+	   Zombie.visit("http://localhost:8443/",
+			             {debug: true},
+			             function (e, browser) {
+
+                     // give up after some time
+                     var timeout = setTimeout(function () {
+                                                console.log("App did not fully load");
+                                                process.exit(1);
+                                              }, secondsToWait * 1000);
+                           console.log("fuck");
+				             // Check frequently to see if the app is loaded, and move forward when it is
+				             var interval = setInterval(
+                       function () {
+                           console.log("fuck");
+									         // add the global objects to our global namespace
+									         enyo = browser.window.enyo;
+                           xcore = browser.window.xcore;
+									         XG = browser.window.XG;
+									         XM = browser.window.XM;
+									         XT = browser.window.XT;
+									         XV = browser.window.XV;
+									         XZ.browser = browser;
+
+									         XT.log = function (message, obj) {
+										         if (message && message.toLowerCase().indexOf("error") === 0) {
+											         // errors from the datasource should cause the test to fail
+											         assert.fail(message + " " + JSON.stringify(obj));
+										         }
+										         // log if verbose mode or if the log is a warning
+										         if (verboseMode || (message && message.code)) {
+											         console.log(JSON.stringify(arguments));
+										         }
+									         };
+
+									         // WIP. Not yet working. Probably need to move it up to earlier app start status.
+                           var oldLoc = XT.String.loc;
+									         XT.String.loc = function (str) {
+										         var localized = XT.localizeString(str);
+										         if (localized === str) {
+											         assert.fail(str + " has no translation");
+										         } else {
+											         oldLoc(str);
+										         }
+									         };
+
+                           // These are annoying
+									         browser.window.Backbone.Relational.showWarnings = false;
+
+									         // clear out both is interval and the I'm-giving-up timeout
+									         // we really want neither to be run again.
+									         clearInterval(interval);
+									         clearTimeout(timeout);
+
+									         // give control back to whoever called us
+									         callback();
+							         }, 100); // 100 = check to see if the app is loaded every 0.1 seconds)
+			             });
+   };
+ }());
--- tests/logo.js
+++ tests/logo.js
@@ -3,10 +3,23 @@
 
   var login = require(__dirname + '/lib/login');
 
-  describe('logo functionality', function (done) {                                                   
-    it('can set the logo source url', function (done) {    
+   before(function (done) {
+            this.timeout(50000);
+            var appLoaded = function () {
+              console.log("App loaded");
+              done();
+            };
+
+            login.loadApp(appLoaded);
+    });
+
+
+  describe('logo functionality', function (done) {
+    it('can set the logo source url', function (done) {
       xCore.$.mainView.setLogoImage('test-url');
-      assertEqual(xCore.$.mainView.$.logo.$.image.getAttribute('src'), 'test-url');
+      assertEqual(xcore.$.mainView.$.logo.$.image.getAttribute('src'), 'test-url');
+
+         done();
     });
   });
-}());       
+}());
