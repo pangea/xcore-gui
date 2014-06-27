a813146807b28ec557ada05a46751afba34b6705
Noah Prince <noahprince8@gmail.com>
2014-06-27 11:01:07 -0500

    test travis

--- test/logo.js
+++ test/logo.js
@@ -2,6 +2,7 @@
   "use strict";
 
   var login = require(__dirname + '/lib/login');
+  var assert = require('assert');
 
    before(function (done) {
             this.timeout(50000);
@@ -9,17 +10,15 @@
               console.log("App loaded");
               done();
             };
-
             login.loadApp(appLoaded);
     });
 
 
   describe('logo functionality', function (done) {
     it('can set the logo source url', function (done) {
-      xCore.$.mainView.setLogoImage('test-url');
-      assertEqual(xcore.$.mainView.$.logo.$.image.getAttribute('src'), 'test-url');
-
-         done();
+      xcore.$.mainView.setLogoImage('test-url');
+      assert.equal(xcore.$.mainView.$.logo.$.image.getAttribute('src'), 'test-url');
+      done();
     });
   });
 }());
