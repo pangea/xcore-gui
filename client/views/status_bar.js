123b646d38c3ac2a2751415c492718f5d64acb06
Bry Zettler <bryanzettler@gmail.com>
2014-07-10 17:56:50 -0500

    added notification

--- client/views/status_bar.js
+++ client/views/status_bar.js
@@ -6,7 +6,9 @@ enyo.kind({
   components: [
     {kind: "onyx.Toolbar", layoutKind: "FittableHeaderLayout", components: [
       {kind: "FittableColumns", fit: true, components: [
-        {kind: "FittableRows", name: "alerts", style: "width: 50%;"},
+        {kind: "FittableRows", name: "alerts", style: "width: 50%;", components: [
+          {kind: "Notification", name: "notif"}
+        ]},
         {kind: "FittableRows", name: "icons", style: "width: 50%;"}
       ]}
     ]}
