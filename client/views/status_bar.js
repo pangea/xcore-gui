866ede8856bc351e012c67de1f96da817518f716
Bry Zettler <bryanzettler@gmail.com>
2014-07-29 15:38:26 -0500

    alert now hidding and scroll bar on left

--- client/views/status_bar.js
+++ client/views/status_bar.js
@@ -22,14 +22,17 @@ enyo.kind({
     this.doStatusBarItemAdded();
   },
   addStatusBarAlertAction: function(inEvent, alert) {
+    console.log(alert);
     var type = alert.type ? alert.type : 'info',
         content = alert.content,
         title = alert.title ? alert.title : '',
         icon = alert.icon ? alert.icon : '' ,
         duration = alert.duration ? alert.duration : undefined,
-        stay = alert.stay ? alert.stay : true,
+        stay = alert.stay == false ? false : true,
         theme = alert.theme ? alert.theme : 'notification.MessageBar';
 
+    console.log(duration);
+    console.log(stay);
     this.$.notif.sendNotification({ 
       type: type,
       title: title,
