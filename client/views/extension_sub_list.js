89383f116b60c6b594347dc8b9d2f6b7c383041c
Christopher Rankin <rankin.devon@gmail.com>
2014-08-29 17:28:11 -0500

    Sidebar fixes

--- client/views/extension_sub_list.js
+++ client/views/extension_sub_list.js
@@ -2,34 +2,7 @@ enyo.kind({
   name: "XV.ExtensionSubList",
   kind: "Panels",
   fit: true,
-  data: [],
   events: {
-    onSubListSelect: ""
   },
-  components: [
-    {name: "list", kind: "List", fit: true, multiSelect: false, classes: "extension-sub-list", onSetupItem: "setupItem", components: [
-			{ name: 'sublist_item', classes: 'item', ontap: 'itemTap', components: [
-				{ name: 'sublist_module' }
-			]}
-    ]}
-  ],
-  create: function() {
-    this.inherited(arguments);
-    this.$.list.count = this.data.length;
-  },
-  setupItem: function(inSender, inEvent) {
-		var idx = inEvent.index;
-		var item = this.data[idx];
-		this.$.sublist_module.setContent(item.name);
-console.log(this.$.sublist_item);
-console.log("--------------------------");
-console.log(this.$);
-    this.$.sublist_item.addRemoveClass("selected", inSender.isSelected(idx));
-    return true;
-  },
-  itemTap: function(inSender, inEvent) {
-		var idx = inEvent.index;
-		this.doSubListSelect(this.data[idx]);
-    this.$.list.select(idx);
-  }
+  components: []
 });
--- client/views/widgets/sublist.js
+++ client/views/widgets/sublist.js
@@ -7,27 +7,29 @@ enyo.kind({
     items: []
   },
   components: [
-    { name: "list",
+    { 
+      name: "list",
       kind: "List",
       fit: true,
       classes: "extension-sub-list",
       onSetupItem: "setupItem",
       components: [
-        { name: "sublistItem", classes: 'item', ontap: "itemTap", components: [
-          { name: "sublistModule" }
-        ] }
+        {
+          name: "sublistItem", classes: 'item', ontap: "itemTap", 
+          components: [
+            { name: "sublistModule" }
+          ] 
+        }
       ]
     }
   ],
   create: function() {
     this.inherited(arguments);
-    // apparently, data bindings don't work for this...
     this.$.list.setCount(this.items.length);
   },
   setupItem: function(inSender, inEvent) {
     var i = inEvent.index,
         item = this.getItems()[i];
-
     this.$.sublistModule.setContent(item.name);
     this.$.sublistItem.addRemoveClass('selected', inSender.isSelected(i));
     return true;
