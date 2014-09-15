489b7b1f773bf703af182d85745469ab89e05ae0
Chris Hall <chall@pangeare.com>
2014-09-15 17:50:50 -0500

    Added real functionality to the User Nav
    Also added functionality to the logout button

--- client/views/user_nav.js
+++ client/views/user_nav.js
@@ -1,16 +1,35 @@
 enyo.kind({
   name: "XV.UserNav",
-  events: {},
   components: [
     {kind: 'onyx.MenuDecorator', classes: "user-nav", components: [
       {content: "User Menu"},
-      {name: 'userMenu', kind: 'onyx.Menu', components: [
-        {content: 'Logout', value: 'logout'}
-      ]}
+      { name: 'userMenu',
+        kind: 'onyx.Menu',
+        ontap: 'handleNavSelect',
+        components: [
+          {content: 'Logout', value: 'logout', method: 'logout'}
+        ]
+      }
     ]}
   ],
   addUserNavAction: function (action) {
     this.$.userMenu.createComponent(action);
     this.$.userMenu.render();
+  },
+  handleNavSelect: function(inSender, inEvent) {
+    var tapped  = inEvent.originator,
+        method  = tapped.method,
+        context = tapped.context;
+
+    if(context) {
+      context[method].call(context);
+    } else {
+      this[method].call(this);
+    }
+
+    return true;
+  },
+  logout: function() {
+    location.assign('/logout');
   }
 });
